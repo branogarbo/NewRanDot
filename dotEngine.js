e903ba294ad348177271aa3cdfc99a45b2359e80
Brian Longmore <43781990+branogarbo@users.noreply.github.com>
2019-12-24 17:40:45 -1000

    Working version with while loop

--- dotEngine.js
+++ dotEngine.js
@@ -1,30 +1,28 @@
-var dotIt = 0;
 var randInt = (lower,upper) => {return Math.floor(Math.random()*(upper-lower)+lower)};
-var coords = "calc(50vw - 50%), calc(50vh - 50%)";
 
-function createDot(fieldRadius,dotRadius) {
-  document.body.insertAdjacentHTML('beforeend',`<div class="dot"></div>`);
-  var fixedRan = randInt(0,360);
+function createDots(dotNums,fieldRadius,dotRadius) {
+  var coords = "calc(50vw - 50%), calc(50vh - 50%)";
+  var dotIt = 0;
   
-  document.querySelectorAll('div.dot')[dotIt].style = `
-    position:absolute;
-    border-radius:50%;
-    background:hsla(${randInt(0,255)},100%,50%,${Math.random()});
-    width:${dotRadius*2}px;
-    height:${dotRadius*2}px;
-    transform:translate(${coords});
-  `;
-  
-  var x = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().left;
-  var y = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().top;
+  while (dotIt<dotNums) {
+    document.body.insertAdjacentHTML('beforeend',`<div class="dot"></div>`);
+    var fixedRan = randInt(0,360);
+    
+    document.querySelectorAll('div.dot')[dotIt].style = `
+      position:absolute;
+      border-radius:50%;
+      background:hsla(${randInt(0,255)},100%,50%,${Math.random()});
+      width:${dotRadius*2}px;
+      height:${dotRadius*2}px;
+      transform:translate(${coords});
+    `;
+    
+    var x = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().left;
+    var y = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().top;
 
-  coords = `${x+Math.random()*fieldRadius*Math.cos(fixedRan*Math.PI/180)}px, ${y+Math.random()*fieldRadius*Math.sin(fixedRan*Math.PI/180)}px`;
-
-  dotIt++;
+    coords = `${x+Math.random()*fieldRadius*Math.cos(fixedRan*Math.PI/180)}px, ${y+Math.random()*fieldRadius*Math.sin(fixedRan*Math.PI/180)}px`;
+    dotIt++;
+  }
 }
 
-function addDots(interval,fieldRadius,dotRadius) {
-  setInterval(createDot,interval,fieldRadius,dotRadius);
-}
-
-addDots(1,20,20);
+createDots(1000,40,20);
