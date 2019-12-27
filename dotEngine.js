84d92bfa1ab3401ee9fdeff8ae836fdbb668125c
Brian Longmore <43781990+branogarbo@users.noreply.github.com>
2019-12-26 18:43:26 -1000

    Added a bunch of things. User control, function that takes evaluatable parameters.

--- dotEngine.js
+++ dotEngine.js
@@ -1,36 +1,99 @@
-var dotIt = 0;
 var randInt = (lower,upper) => {return Math.floor(Math.random()*(upper-lower)+lower)};
-var coords = "calc(50vw - 50%), calc(50vh - 50%)";
+var userInput = window.prompt(`
+  Keyboard features:
+    p : pauses/unpauses dot engine
+    r : restarts engine with the same parameters
+    c : restarts engine, allows users to change parameters
+    q : removes last dot
 
-function createDot(fieldRadius,dotRadius) {
-  document.body.insertAdjacentHTML('beforeend',`<div class="dot"></div>`);
-  var fixedRan = randInt(0,360);
+  addDot(dot quantity, time between dots, dot field radius, dot radius)
+`,'addDot(Infinity,50,100,50)');
+
+runEngine();
+
+function runEngine() {
+  // initializing values
+  document.getElementById('dotCont').outerHTML = '<div id="dotCont"></div>';
+
+  var dotIt = 0;
+  var coords = "calc(50vw - 50%), calc(50vh - 50%)";
   
-  document.querySelectorAll('div.dot')[dotIt].style = `
-    position:absolute;
-    border-radius:50%;
-    background:hsla(${randInt(0,255)},100%,50%,${Math.random()});
-    width:${dotRadius*2}px;
-    height:${dotRadius*2}px;
-    transform:translate(${coords});
-  `;
+  function createDot(fieldRadius,dotRadius) {
+    document.getElementById('dotCont').insertAdjacentHTML('beforeend',`<div class="dot"></div>`); // creating dot
+    var fixedRan = randInt(0,360);
+    
+    // applying style to dot (position,color,size)
+    document.querySelectorAll('div.dot')[dotIt].style = `
+      position:absolute;
+      border-radius:50%;
+      background:hsla(${randInt(0,255)},100%,50%,${Math.random()*(0.9)+0.1});
+      width:${dotRadius*2}px;
+      height:${dotRadius*2}px;
+      transform:translate(${coords});
+    `;
+      
+    // storing postion relative to page border
+    var x = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().left;
+    var y = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().top;
+    
+    // adding vector to dots position to get position of next dot
+    coords = `${x+Math.random()*fieldRadius*Math.cos(fixedRan*Math.PI/180)}px, ${y+Math.random()*fieldRadius*Math.sin(fixedRan*Math.PI/180)}px`;
+    
+    dotIt++;
+  }
   
-  var x = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().left;
-  var y = document.querySelectorAll('div.dot')[dotIt].getBoundingClientRect().top;
+  // does stuff when key is pressed
+  var pause = false;
 
-  coords = `${x+Math.random()*fieldRadius*Math.cos(fixedRan*Math.PI/180)}px, ${y+Math.random()*fieldRadius*Math.sin(fixedRan*Math.PI/180)}px`;
+  document.onkeypress = event => {
+    // r : restarts engine with the same parameters
+    if (event.key == "r") {
+      pause = true;
+      runEngine();
+    }
 
-  dotIt++;
+    // c : restarts engine, allows users to change parameters
+    if (event.key == "c") {
+      userInput = window.prompt('addDot(# of dots, time between dots, dot field radius, dot radius)',userInput);
+      pause = true;
+      runEngine();
+    }
+
+    // p : pauses/unpauses engine 
+    if (event.key == "p" && pause == false) {pause = true}
+    else if (event.key != "p" && pause == true) {}
+    else {pause = false}
+
+    // q : removes last dot (not complete)
+    if (event.key == "q" && document.querySelectorAll('div.dot').length != 0) {
+      var lastPos = document.querySelectorAll('div.dot').length-1;
+      x = document.querySelectorAll('div.dot')[lastPos-1].getBoundingClientRect().left;
+      y = document.querySelectorAll('div.dot')[lastPos-1].getBoundingClientRect().top;
+
+      if (dotIt > 0) {
+        document.querySelectorAll('div.dot')[lastPos].outerHTML = "";
+        dotIt--;
+      }
+    }
+
+    // question mark notation doesn't work for some reason
+  
+    //event.key == "p" && pause == false ? pause = true :
+    //event.key =! "p" && pause == true ? ()=>{} :
+    //pause = false;
+  };
+
+  // adds styled dot in intervals, takes evaluatable parameters
+  function addDot(dotMax,interval,fieldRadius,dotRadius) {
+    var dotR = eval(dotRadius);
+    var fieldR = eval(fieldRadius);
+    var ntrvl = eval(interval);
+    var dotM = eval(dotMax);
+
+    pause == false ? createDot(fieldR,dotR) : ()=>{};
+    dotIt < dotM ? setTimeout(addDot,ntrvl,dotMax,interval,fieldRadius,dotRadius) : ()=>{};
+  }  
+
+  // runs users input as code
+  eval(userInput);
 }
-
-function addDots(dotMax,interval,fieldRadius,dotRadius) {
-  var dotR = eval(dotRadius);
-  var fieldR = eval(fieldRadius);
-  var ntrvl = eval(interval);
-  var dotM = eval(dotMax);
-
-  createDot(fieldR,dotR);
-  dotIt < dotM ? setTimeout(addDots,ntrvl,dotMax,interval,fieldRadius,dotRadius) : ()=>{};
-}
-
-addDots(Infinity,1,80,40);
